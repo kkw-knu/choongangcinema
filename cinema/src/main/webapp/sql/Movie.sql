69de8fb9fc4ca6b51f5727f2b74455f808a6ac2d
kkw-knu <rlarjsdn529@naver.com>
2021-08-25 19:39:02 +0900

    sql

--- cinema/src/main/webapp/sql/Movie.sql
+++ cinema/src/main/webapp/sql/Movie.sql
@@ -1,19 +1,18 @@
 create table movie (
-   mv_num number primary key,          --영화코드
-   mv_title varchar2(50) not null,      --제목
-   mv_director varchar2(30) not null,   --감독
-   mv_genre varchar2(50) not null,      --장르
-   mv_grade varchar2(20) not null,      --관람등급
-   mv_img varchar2(100) not null,      --영화포스터
-   mv_content varchar2(1024) not null,   --영화줄거리
-   mv_startdate date not null,         --개봉일자
-   mv_time number not null            --상영시간(분)
+   mv_num number primary key,      
+   mv_title varchar2(50) not null,      
+   mv_director varchar2(30) not null,  
+   mv_genre varchar2(50) not null,     
+   mv_grade varchar2(20) not null,     
+   mv_img varchar2(100) not null,    
+   mv_content varchar2(1024) not null,   
+   mv_startdate date not null,       
+   mv_time number not null            
 );
 select * from MOVIE;
 
---��ȭ ���� ���̺� 
 drop table movie;
 
---ORA-02449: unique/primary keys in table referenced by foreign keys (0 rows affected) 뜨면 이렇게
+--ORA-02449: unique/primary keys in table referenced by foreign keys (0 rows affected)
 -- drop table TABLE_NAME cascade constraints;
 drop table movie cascade constraints;
--- cinema/src/main/webapp/sql/Mvlike.sql
+++ cinema/src/main/webapp/sql/Mvlike.sql
@@ -1,7 +1,7 @@
---영화 좋아요를 위한 테이블
+
 create table mvlike (
-	like_id varchar2(30) not null, --좋아요한 아이디
-	like_mv_num number not null,	--좋아요한 영화번호
-	like_id_num number not null --좋아요한 아이디의 num (java 한글 안넘어가는 부분떄문에 만듬)
+	like_id varchar2(30) not null,
+	like_mv_num number not null,	
+	like_id_num number not null 
 );
 drop table mvlike;
